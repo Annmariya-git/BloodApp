5697fde1d177a5c6e862385e23dcb4865dd2c40e
Annmariya-git <annmariyavarghese2027@gmail.com>
2026-07-13 09:56:19 +0530

    path

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,22 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddDonor from './components/AddDonor'
+import ViewAllDonors from './components/ViewAllDonors'
+import NavDonors from './components/NavDonors'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <AddDonor />
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<AddDonor />}/>
+    <Route path='/View' element={<ViewAllDonors />}/>
+
+   </Routes>
+   </BrowserRouter>
     </>
   )
 }
