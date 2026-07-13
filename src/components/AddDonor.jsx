493692ac00869d219e6e60cb051a6acb6993f9ab
Annmariya-git <annmariyavarghese2027@gmail.com>
2026-07-13 09:33:45 +0530

    edit

--- src/components/AddDonor.jsx
+++ src/components/AddDonor.jsx
@@ -8,40 +8,40 @@ const AddDonor = () => {
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <div className="row">
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label"></label>
+                            <label htmlFor="" className="form-label">Donor Name</label>
                             <input type="text" className="form-control" />
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label"></label>
+                            <label htmlFor="" className="form-label">Age</label>
+                            <input type="num" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">gender</label>
                             <input type="text" className="form-control" />
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label"></label>
+                            <label htmlFor="" className="form-label">BloodGroup</label>
                             <input type="text" className="form-control" />
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label"></label>
+                            <label htmlFor="" className="form-label">Phone</label>
+                            <input type="num" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Email</label>
+                            <input type="email" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">City</label>
                             <input type="text" className="form-control" />
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label"></label>
+                            <label htmlFor="" className="form-label">Weight-kg</label>
                             <input type="text" className="form-control" />
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label"></label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label"></label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label"></label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label"></label>
-                            <input type="text" className="form-control" />
+                            <label htmlFor="" className="form-label">last-dontion-date</label>
+                            <input type="date" name="" id="" className="form-control" />
                         </div>
                         <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <button className="btn btn-success">Submit</button>
